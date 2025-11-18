dddbab558bf0f8e861c1688318de89f98c3b1ad2
TimDM17 <TimDucMinh@gmx.de>
2025-11-18 12:06:50 +0100

    adjusting setup and adding gitignore

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-from setuptools import setup, find_packages
-
 # Read requirements from requirements.txt
 with open("requirements.txt", "r") as f:
     requirements = [line.strip() for line in f if line.strip() and not line.startswith("#")]

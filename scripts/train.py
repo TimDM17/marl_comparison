def65c38b2d3748f8863a54511276dd1a8be0891
TimDM17 <TimDucMinh@gmx.de>
2025-11-19 11:34:38 +0100

    Add CUDA Seed Setting

--- scripts/train.py
+++ scripts/train.py
@@ -31,8 +31,10 @@ def set_seed(seed: int):
     np.random.seed(seed)
     torch.manual_seed(seed)
     if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
-
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False      
 
 def create_agent(config: dict, env: MaMuJoCoWrapper):
     """Create agent based on config."""
